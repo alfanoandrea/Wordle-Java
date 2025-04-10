84a2eae1e27e950f2b49ab06d750d72afcfbb048
alfanowski <andrea.alfano2006@gmail.com>
2025-04-10 09:45:04 +0200

    implementazione logica iniziale

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,47 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("wordle");
+        String parola = parolaCasuale("dictionaries/dictionaryIT.txt");
+        System.out.println(parola);
+        Wordle partita = new Wordle(parola);
 
+        for (int i = 0; i < 6; i++) {
+            String tentativo = JOptionPane.showInputDialog("Tentativo " + (i + 1) + ":");
+            partita.setTentativo(tentativo);
+            if (!partita.controlloTentativo()) {
+                i--;
+                continue;
+            }
+            System.out.println(tentativo);
+            System.out.println(partita.controlloColori());
+            if (partita.controlloVittoria()) {
+                System.out.println("Hai vinto");
+                return;
+            }
+        }
+        System.out.println("Hai perso");
+    }
+
+    public static String parolaCasuale(String fileName) {
+        try (BufferedReader bw = new BufferedReader(new FileReader(new File(fileName)))) {
+            String line;
+            int lines = Files.readAllLines(Paths.get(fileName)).size();
+            int randomLine = (int) (Math.random() * lines);
+            int i = 0;
+            while ((line = bw.readLine()) != null)
+                if (i++ == randomLine)
+                    break;
+            bw.close();
+            return line;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
--- src/Wordle.java
+++ src/Wordle.java
@@ -1,9 +1,59 @@
 class Wordle {
-    String parola, tentativo;
+    private String parola, tentativo;
 
-    public Wordle(String parola, String tentativo) {
+
+    public Wordle(String parola) {
         this.parola = parola;
+        }
+
+    public Wordle() {
+        this.parola = "";
+        this.tentativo = "";
+    }
+
+
+    public String getParola() {
+        return parola;
+    }
+
+    public void setParola(String parola) {
+        this.parola = parola;
+    }
+
+    public String getTentativo() {
+        return tentativo;
+    }
+
+    public void setTentativo(String tentativo) {
         this.tentativo = tentativo;
     }
+
+
+    public boolean controlloVittoria() {
+        return parola.toLowerCase().equals(tentativo.toLowerCase());
+    }
+
+    public boolean controlloTentativo() {
+        return tentativo.length() == 5;
+    }
+
+    public String controlloColori() {
+        String s = "NNNNN";
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (tentativo.charAt(i) == parola.charAt(j))
+                    if (i == j) {
+                        s = s.substring(0, i) + "G" + s.substring(i + 1);
+                        break;
+                    } else {
+                        s = s.substring(0, i) + "Y" + s.substring(i + 1);
+                        break;
+                    }  
+            }
+        }        
+        return s;
+    }
+
+
 
 }
