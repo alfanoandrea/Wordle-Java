2b8e412cf4e03d107eef8c23ad9b5592904bdd36
onur_ <76625957+onurjkk@users.noreply.github.com>
2025-05-11 18:48:15 +0200

    cambiato tutto

--- src/Wordle.java
+++ src/Wordle.java
@@ -1,58 +1,11 @@
-class Wordle {
-    private String parola, tentativo;
+package wordle;
 
 
-    public Wordle(String parola) {
-        this.parola = parola;
-        }
+public class Wordle {
 
-    public Wordle() {
-        this.parola = "";
-        this.tentativo = "";
+  
+    public static void main(String[] args) {
+        WordleController controller = new WordleController(new GUI(), new WordleModel());
     }
-
-
-    public String getParola() {
-        return parola;
-    }
-
-    public void setParola(String parola) {
-        this.parola = parola;
-    }
-
-    public String getTentativo() {
-        return tentativo;
-    }
-
-    public void setTentativo(String tentativo) {
-        this.tentativo = tentativo;
-    }
-
-
-    public boolean controlloVittoria() {
-        return parola.toLowerCase().equals(tentativo.toLowerCase());
-    }
-
-    public boolean controlloTentativo() {
-        return tentativo.length() == 5;
-    }
-
-    public String controlloColori() {
-        String s = "NNNNN";
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (tentativo.charAt(i) == parola.charAt(j))
-                    if (i == j) {
-                        s = s.substring(0, i) + "G" + s.substring(i + 1);
-                        break;
-                    }
-                    else
-                        s = s.substring(0, i) + "Y" + s.substring(i + 1);  
-            }
-        }        
-        return s;
-    }
-
-
-
+    
 }
